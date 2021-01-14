b605f1e3d40a1ce96dc223fc9efd637051203878
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2021-01-14 23:56:24 +0530

    Complete Tic-tac-toe application code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import calculateWinner from './calculateWinner';
+import Panel from './Panel';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current =history[history.length - 1];
+    const squares = current.squares.slice();
+   // console.log(squares);
+   if (calculateWinner(squares) || squares[i]) {
+       return;
+   }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+}
+
+  jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status ='Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div className="app">
+        <div  className="app-panel">
+            <Panel 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+              />
+        </div>
+        <div className="app-info">
+            <div>{status}</div>           
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
